85420b6c313e6c48ffa6d258cd63a14411a45893
Prasanna <pum547@gmail.com>
2020-08-21 17:12:04 +0530

    Webdriver demo added

---
+++ src/SeleniumDay1/DemoRadioCheckbox.java
@@ -0,0 +1,25 @@
+package SeleniumDay1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class DemoRadioCheckbox {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");  //"C:\Selenium\chromedriver.exe"
+
+        // Instantiate a ChromeDriver class.
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://demo.guru99.com/test/radio.html");
+        driver.findElement(By.id("vfb-7-1")).click(); //radio button 1 selected
+        Thread.sleep(5000);
+        System.out.println("Radio Button Selected");
+        driver.findElement(By.id("vfb-6-0"));  //Check Box 1 selected
+        driver.quit();
+    }
+}
