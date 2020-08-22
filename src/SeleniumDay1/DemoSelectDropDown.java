094e91d00eb25b263f500412261d2362ddf61a77
Prasanna <pum547@gmail.com>
2020-08-22 10:49:10 +0530

    Demo dropdown select added

--- src/SeleniumDay1/DemoSelectDropDown.java
+++ src/SeleniumDay1/DemoSelectDropDown.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class DemoSelect {
+public class DemoSelectDropDown {
     public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");  //"C:\Selenium\chromedriver.exe"
